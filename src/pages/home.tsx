134ae5fdfd66763aea4754c6b4d3672833535b2a
TheGreatRambler <tgr@tgrcode.com>
2024-02-09 21:38:51 -0700

    Add threejs skeleton

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,34 @@
-import { createSignal } from 'solid-js';
+import { createSignal, createEffect, } from 'solid-js';
+import * as THREE from 'three';
 
 export default function Home() {
   const [count, setCount] = createSignal(0);
 
+  let mapContainer: HTMLDivElement;
+  createEffect(() => {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    mapContainer.appendChild(renderer.domElement);
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+  }, []);
   return (
+
     <section class="bg-gray-100 text-gray-700 p-8">
       <h1 class="text-2xl font-bold">Home</h1>
       <p class="mt-4">This is the home page.</p>
@@ -24,7 +49,10 @@ export default function Home() {
         >
           +
         </button>
+
+        <div ref={mapContainer}></div>
       </div>
     </section>
+
   );
 }

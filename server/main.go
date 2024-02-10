fc7d1616c499d1af1b98d7359a4c6b5300ad4668
TheGreatRambler <tgr@tgrcode.com>
2024-02-10 15:06:30 -0700

    Temporary event RE work

--- server/main.go
+++ server/main.go
@@ -7,13 +7,15 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 // Port we listen on.
 const portNum string = ":3000"
 
 type Server struct {
-	HttpClient http.Client
+	HttpClient    http.Client
+	HomepageRegex *regexp.Regexp
 }
 
 type MinesEventsPayload struct {
@@ -28,6 +30,49 @@ type MinesEventsPayload struct {
 }
 
 func (s *Server) GetEvents(w http.ResponseWriter, r *http.Request) {
+	homepage_req, err := http.NewRequest("GET", fmt.Sprintf("https://mines.emscloudservice.com/web/BrowseEvents.aspx"), nil)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	homepage_req.Header.Set("Origin", "https://mines.emscloudservice.com")
+	homepage_req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0")
+	homepage_req.Header.Set("Host", "mines.emscloudservice.com")
+	homepage_req.Header.Set("Sec-Fetch-Dest", "document")
+	homepage_req.Header.Set("Sec-Fetch-Mode", "navigate")
+	homepage_req.Header.Set("Sec-Fetch-Site", "none")
+	homepage_req.Header.Set("Sec-Fetch-User", "?1")
+	homepage_req.Header.Set("Upgrade-Insecure-Requests", "1")
+
+	homepage_req.AddCookie(&http.Cookie{
+		Name:  "OptanonConsent",
+		Value: "consentId=ca5945b7-bcc1-46be-86eb-bf2b4a85ee1b&datestamp=Sat+Feb+10+2024+14%3A49%3A17+GMT-0700+(Mountain+Standard+Time)&version=6.33.0&interactionCount=0",
+	})
+
+	// Perform request
+	res, err := s.HttpClient.Do(homepage_req)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	homepage_body, err := io.ReadAll(res.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	fmt.Println("BODY %s", string(homepage_body))
+
+	homepageTokenMatch := s.HomepageRegex.Find(homepage_body)
+
+	fmt.Println("TEST1 %s", homepageTokenMatch)
+
+	homepageToken := homepageTokenMatch[25 : len(homepageTokenMatch)-1]
+
+	fmt.Println("TEST %s", homepageToken)
+
 	postBody, _ := json.Marshal(MinesEventsPayload{
 		FilterData: struct {
 			Filters []struct {
@@ -79,8 +124,30 @@ func (s *Server) GetEvents(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	elevation_req.Header.Set("Origin", "https://mines.emscloudservice.com")
+	elevation_req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0")
+	elevation_req.Header.Set("Host", "mines.emscloudservice.com")
+	elevation_req.Header.Set("dea-CSRFToken", string(homepageToken))
+
+	elevation_req.AddCookie(&http.Cookie{
+		Name:  "ASP.NET_SessionId",
+		Value: "0pr4kcnfvzyfaav3v40bsjla",
+	})
+	elevation_req.AddCookie(&http.Cookie{
+		Name:  "__AntiXsrfToken",
+		Value: "54a688b2eae94276990379154b1b053e",
+	})
+	elevation_req.AddCookie(&http.Cookie{
+		Name:  "OptanonAlertBoxClosed",
+		Value: "2024-02-10T20:53:38.110Z",
+	})
+	elevation_req.AddCookie(&http.Cookie{
+		Name:  "OptanonConsent",
+		Value: "isGpcEnabled=0&datestamp=Sat+Feb+10+2024+13%3A53%3A38+GMT-0700+(Mountain+Standard+Time)&version=6.33.0&isIABGlobal=false&hosts=&consentId=0410ffad-6a07-4daf-9ee7-4688c8cc537c&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1&geolocation=US%3BCO&AwaitingReconsent=false",
+	})
+
 	// Perform request
-	res, err := s.HttpClient.Do(elevation_req)
+	res, err = s.HttpClient.Do(elevation_req)
 	if err != nil {
 		w.WriteHeader(500)
 		return
@@ -99,13 +166,19 @@ func (s *Server) GetEvents(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.Println("Starting")
 
+	homepage_token, err := regexp.Compile("id=\"deaCSRFToken\" value=\"[^\"]+\"")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s := &Server{
-		HttpClient: http.Client{},
+		HttpClient:    http.Client{},
+		HomepageRegex: homepage_token,
 	}
 
 	http.HandleFunc("/get_events", s.GetEvents)
 
-	err := http.ListenAndServe(portNum, nil)
+	err = http.ListenAndServe(portNum, nil)
 	if err != nil {
 		log.Fatal(err)
 	}

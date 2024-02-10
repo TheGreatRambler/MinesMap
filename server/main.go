fe03513a9882835b14d44d20bc49bb4d17d0a810
TheGreatRambler <tgr@tgrcode.com>
2024-02-10 15:13:26 -0700

    Remove cookie and add header

--- server/main.go
+++ server/main.go
@@ -44,11 +44,14 @@ func (s *Server) GetEvents(w http.ResponseWriter, r *http.Request) {
 	homepage_req.Header.Set("Sec-Fetch-Site", "none")
 	homepage_req.Header.Set("Sec-Fetch-User", "?1")
 	homepage_req.Header.Set("Upgrade-Insecure-Requests", "1")
+	homepage_req.Header.Set("Connection", "keep-alive")
 
-	homepage_req.AddCookie(&http.Cookie{
-		Name:  "OptanonConsent",
-		Value: "consentId=ca5945b7-bcc1-46be-86eb-bf2b4a85ee1b&datestamp=Sat+Feb+10+2024+14%3A49%3A17+GMT-0700+(Mountain+Standard+Time)&version=6.33.0&interactionCount=0",
-	})
+	/*
+		homepage_req.AddCookie(&http.Cookie{
+			Name:  "OptanonConsent",
+			Value: "consentId=ca5945b7-bcc1-46be-86eb-bf2b4a85ee1b&datestamp=Sat+Feb+10+2024+14%3A49%3A17+GMT-0700+(Mountain+Standard+Time)&version=6.33.0&interactionCount=1&isGpcEnabled=0&isIABGlobal=false&hosts=&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1; ASP.NET_SessionId=zkrjbhfo3zubsjwn2em0ftk2; __AntiXsrfToken=47f02afecaa542bcbf8d23d070e15207; OptanonAlertBoxClosed=2024-02-10T22:03:57.527Z",
+		})
+	*/
 
 	// Perform request
 	res, err := s.HttpClient.Do(homepage_req)

f36805b5fbf78d97b89b3305f3756222219c4efa
= <=>
2024-02-10 02:27:13 -0700

    cap fps and other stuff

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,51 @@
 import { createSignal, createEffect, } from 'solid-js';
 import * as THREE from 'three';
-// import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
-// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+
+// class Floor {
+//   constructor(){
+//     this.
+//   }
+// }
+
+// class Building { 
+//   currFloor : number;
+//   floors : Floor[];
+//   defaultFloor : number;
+//   constructor(floors, defaultFloor){
+//     this.floors = floors;
+//     this.defaultFloor = defaultFloor;
+//     this.currFloor = defaultFloor;
+//   }
+// }
+
 export default function Home() {
-  // const [count, setCount] = createSignal(0);
+  const [currFloor, setCurrFloor] = createSignal(0);
+
+  const toggleFloor = () => {
+    setCurrFloor(1-currFloor());
+  }
 
   let mapContainer: HTMLDivElement;
   createEffect(() => {
 
+    // scene
     var scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xffffff );
 
+    // camera
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.y = 5
     camera.rotation.x = -75;
 
+    // renderer
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(mapContainer.clientWidth, mapContainer.clientHeight);
     mapContainer.appendChild(renderer.domElement);
 
+    // controls
     var controls = new OrbitControls(camera, renderer.domElement);
     controls.mouseButtons = {
       LEFT: THREE.MOUSE.PAN,
@@ -31,6 +55,12 @@ export default function Home() {
     controls.minDistance = 2.5;
     controls.maxDistance = 10;
 
+    // cap fps
+    let clock = new THREE.Clock();
+    let delta = 0;
+    let interval = 1 / 30; // 30 fps
+    
+    // axis guide
     const axesHelper = new THREE.AxesHelper( 5 );
     scene.add( axesHelper );
 
@@ -42,22 +72,28 @@ export default function Home() {
       gltf.scene.position.x = 5;
       scene.add(gltf.scene);
     }, undefined, function (error) {console.error(error);});
-
+    console.log("hello");
     // light
     const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
     scene.add( directionalLight );
     
     var animate = function () {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-      controls.update();
+      delta += clock.getDelta();
+      if (delta  > interval) {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        controls.update();
+        delta = delta % interval;
+      }
     };
     animate();
 
   }, []);
   return (
-
-      <div class="h-full w-full" ref={mapContainer}></div>
+      <div>
+        <button onClick={toggleFloor}>hello</button>
+        <div class="h-full w-full" ref={mapContainer}></div>
+      </div>
 
   );
 }

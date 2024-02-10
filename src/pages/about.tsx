d78dc6e7b7d9a7ebc134646eb75296142de1fcba
TheGreatRambler <tgr@tgrcode.com>
2024-02-09 21:28:34 -0700

    Add solidjs template

---
+++ src/pages/about.tsx
@@ -0,0 +1,26 @@
+import { Component, createEffect, Suspense } from 'solid-js';
+import { useRouteData } from '@solidjs/router';
+import type { AboutDataType } from './about.data';
+
+export default function About() {
+  const name = useRouteData<AboutDataType>();
+
+  createEffect(() => {
+    console.log(name());
+  });
+
+  return (
+    <section class="bg-pink-100 text-gray-700 p-8">
+      <h1 class="text-2xl font-bold">About</h1>
+
+      <p class="mt-4">A page all about this website.</p>
+
+      <p>
+        <span>We love</span>
+        <Suspense fallback={<span>...</span>}>
+          <span>&nbsp;{name()}</span>
+        </Suspense>
+      </p>
+    </section>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [
+    /* 
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin(),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
